78ff22218c109cf064ca53d12f8edc61d8f157d4
Oliver Boudet <oboudet1@me.com>
2018-09-10 16:14:07 +0200

    initial commit

---
+++ PrimeBin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PrimeBin {
+    internal class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("starting");
+            File.Delete("output.txt");
+            BigInteger   i    = 3;
+            StreamWriter file = new StreamWriter("output.txt", true);
+            while (true) {
+                CheckPrime(i, file);
+                i += 2;
+            }
+        }
+
+        public static async void CheckPrime(BigInteger number, StreamWriter file) {
+            if (await IsPrime(number)) {
+                file.WriteLine(number);
+            }
+        }
+
+        public static async Task<bool> IsPrime(BigInteger number) {
+            double boundary = Math.Floor((double)sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static BigInteger sqrt(BigInteger number) {
+            return BigInteger.Pow(number, 1 / 2);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#### PrimeBin
+i think the precompiled version is in /PrimeBin/bin/Debug/
